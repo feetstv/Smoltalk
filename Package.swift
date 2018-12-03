b02a2c3be9549bd1818fc9c13a56629b99749345
Jaesan Ryfle-Turi <jaesan@icloud.com>
2018-12-04 00:27:34 +1300

    1.1.0

--- Package.swift
+++ Package.swift
@@ -23,12 +23,14 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Smoltalk",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources/Smoltalk")
         .testTarget(
             name: "SmoltalkTests",
             dependencies: ["Smoltalk"]),
         .target(
             name: "Terp",
-            dependencies: ["Smoltalk"])
+            dependencies: ["Smoltalk"],
+            path: "Sources/Terp")
     ]
 )
